36387adbda15dbb812489e05089ec41795880a74
wuleiaty <1046828876@qq.com>
2019-09-28 12:39:56 +0800

    bugs fix and change labels

--- anti-deform.py
+++ anti-deform.py
@@ -4,24 +4,24 @@ import time
 import os
 import math
 
-def from_label_deform(label_csv_path: list, img_path):
-    assert os.path.exists(label_csv_path[0]) and os.path.exists(img_path)
+def from_label_deform(label_path, img_path):
+    assert os.path.exists(label_path) and os.path.exists(img_path)
 
-    label_x = np.loadtxt(label_csv_path[0])
-    label_y = np.loadtxt(label_csv_path[1])
-
+    # label_x = np.loadtxt(label_csv_path[0])
+    # label_y = np.loadtxt(label_csv_path[1])
+    label = np.load(label_path)
 
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
     rows, cols, _ = img.shape
-    assert (rows, cols) == label_x.shape, 'shape of labels and img is not same'
+    assert (rows, cols, 2) == label.shape, 'shape of labels and img is not same'
 
     dst_img = np.ones(img.shape) * 255
 
     for x in range(rows):
         for y in range(cols):
-            src_x = x - label_x[x][y]
-            src_y = y - label_y[x][y]
+            src_x = x - label[x][y][0]
+            src_y = y - label[x][y][1]
 
             if(src_x < 0 or src_x >= rows - 1) or (src_y < 0 or src_y >= cols - 1):
                 continue
@@ -39,10 +39,10 @@ def from_label_deform(label_csv_path: list, img_path):
                           + img[ceil_x][floor_y][i] * (src_x - floor_x) * (ceil_y - src_y)\
                           + img[ceil_x][ceil_y][i] * (src_x - floor_x) * (src_y - floor_y))
     
-    return img
+    return dst_img
 
 if __name__ == '__main__':
     start = time.time()
-    img = from_label_deform(['data_gen/labels/1_x.csv', 'data_gen/labels/1_y.csv'], 'data_gen/scan/1.png')
-    cv2.imwrite('data_gen/img/1_anti.png', img)
+    img = from_label_deform('data_gen/labels/dtd_0062.npy', 'data_gen/scan/dtd_0062.jpg')
+    cv2.imwrite('data_gen/img/dtd_0062_anti.jpg', img)
     print(f'time:{time.time() - start}')
