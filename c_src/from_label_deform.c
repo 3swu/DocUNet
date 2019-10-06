fe23b5e94664b728bad19e0f0535aae8bbd146bd
wuleiaty <1046828876@qq.com>
2019-10-06 21:26:53 +0800

    refactor

--- c_src/from_label_deform.c
+++ c_src/from_label_deform.c
@@ -1,52 +1,62 @@
 #include <math.h>
-
-void from_label_deform(float* label_x, float* label_y, 
-                        float* src_img_b, float* src_img_g, float* src_img_r, 
-                        float* dst_img_b, float* dst_img_g, float* dst_img_r, 
-                        int* shape) {
-    int rows = shape[0], cols = shape[1];
-
-    int i, j;
-    float src_x, src_y;
-    int ceil_x, ceil_y, floor_x, floor_y;
+#define EPSINON 0.0001
 
 
-    for (i = 0; i < rows; i++) {
-        for (j = 0; j < cols; j++) {
-            src_x = i - label_x[i * cols + j];
-            src_y = j - label_y[i * cols + j];
 
-            if ((src_x < 0 || src_x >= rows - 1) || (src_y < 0 || src_y >= cols - 1))
+void from_label_deform(double* label_x, double* label_y, int* label_dst_shape,
+                       double* src_img_b, double* src_img_g, double* src_img_r, 
+                       double* dst_img_b, double* dst_img_g, double* dst_img_r,
+                       int* src_img_shape) {
+    
+    int dst_rows = label_dst_shape[0], dst_cols = label_dst_shape[1];
+    int src_rows = src_img_shape[0], src_cols = src_img_shape[1];
+
+    double src_x, src_y;
+    int floor_x, ceil_x, floor_y, ceil_y;
+    int offset_x = (int)(src_rows * 0.25);
+    int offset_y = (int)(src_cols * 0.25);
+
+    for(int i = 0; i < dst_rows; i++) {
+        for(int j = 0; j < dst_cols; j++) {
+            if(label_x[i * dst_cols + j] > -EPSINON && label_x[i * dst_cols + j] < EPSINON)
                 continue;
             else {
-                ceil_x = (int)ceilf(src_x);
-                ceil_y = (int)ceilf(src_y);
-                floor_x = (int)floorf(src_x);
-                floor_y = (int)floorf(src_y);
+                src_x = i - label_x[i * dst_cols + j] - offset_x;
+                src_y = j - label_y[i * dst_cols + j] - offset_y;
 
-                dst_img_b[i * cols + j] = (int)floorf(
-                    src_img_b[floor_x * cols + floor_y] * (ceil_x - src_x) * (ceil_y - src_y) +
-                    src_img_b[floor_x * cols + ceil_y] * (ceil_x - src_x) * (src_y - floor_y) +
-                    src_img_b[ceil_x * cols + floor_y] * (src_x - floor_x) * (ceil_y - src_y) +
-                    src_img_b[ceil_x * cols + ceil_y] * (src_x - floor_x) * (src_y - floor_y)
-                );
+                if((src_x < 0 || src_x >= src_rows - 1) || (src_y < 0 || src_y >= src_cols - 1))
+                    continue;
+                else {
+                    ceil_x = (int)ceil(src_x);
+                    ceil_y = (int)ceil(src_y);
+                    floor_x = (int)floor(src_x);
+                    floor_y = (int)floor(src_y);
 
-                dst_img_g[i * cols + j] = (int)floorf(
-                    src_img_g[floor_x * cols + floor_y] * (ceil_x - src_x) * (ceil_y - src_y) +
-                    src_img_g[floor_x * cols + ceil_y] * (ceil_x - src_x) * (src_y - floor_y) +
-                    src_img_g[ceil_x * cols + floor_y] * (src_x - floor_x) * (ceil_y - src_y) +
-                    src_img_g[ceil_x * cols + ceil_y] * (src_x - floor_x) * (src_y - floor_y)
-                );
+                    dst_img_b[i * dst_cols + j] = (int)floor(
+                        src_img_b[floor_x * src_cols + floor_y] * (ceil_x - src_x) * (ceil_y - src_y) +
+                        src_img_b[floor_x * src_cols + ceil_y] * (ceil_x - src_x) * (src_y - floor_y) +
+                        src_img_b[ceil_x * src_cols + floor_y] * (src_x - floor_x) * (ceil_y - src_y) +
+                        src_img_b[ceil_x * src_cols + ceil_y] * (src_x - floor_x) * (src_y - floor_y)
+                    );
 
-                dst_img_r[i * cols + j] = (int)floorf(
-                    src_img_r[floor_x * cols + floor_y] * (ceil_x - src_x) * (ceil_y - src_y) +
-                    src_img_r[floor_x * cols + ceil_y] * (ceil_x - src_x) * (src_y - floor_y) +
-                    src_img_r[ceil_x * cols + floor_y] * (src_x - floor_x) * (ceil_y - src_y) +
-                    src_img_r[ceil_x * cols + ceil_y] * (src_x - floor_x) * (src_y - floor_y)
-                );
+                    dst_img_g[i * dst_cols + j] = (int)floor(
+                        src_img_g[floor_x * src_cols + floor_y] * (ceil_x - src_x) * (ceil_y - src_y) +
+                        src_img_g[floor_x * src_cols + ceil_y] * (ceil_x - src_x) * (src_y - floor_y) +
+                        src_img_g[ceil_x * src_cols + floor_y] * (src_x - floor_x) * (ceil_y - src_y) +
+                        src_img_g[ceil_x * src_cols + ceil_y] * (src_x - floor_x) * (src_y - floor_y)
+                    );
+
+                    dst_img_r[i * dst_cols + j] = (int)floor(
+                        src_img_r[floor_x * src_cols + floor_y] * (ceil_x - src_x) * (ceil_y - src_y) +
+                        src_img_r[floor_x * src_cols + ceil_y] * (ceil_x - src_x) * (src_y - floor_y) +
+                        src_img_r[ceil_x * src_cols + floor_y] * (src_x - floor_x) * (ceil_y - src_y) +
+                        src_img_r[ceil_x * src_cols + ceil_y] * (src_x - floor_x) * (src_y - floor_y)
+                    );
+
+                }
             }
         }
     }
+
+
 }
-
-
