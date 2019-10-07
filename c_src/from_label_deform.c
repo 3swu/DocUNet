b5bc73dd92a91dadd28ea93c67e8050151655d0d
wuleiaty <1046828876@qq.com>
2019-10-07 18:37:16 +0800

    add image edge

--- c_src/from_label_deform.c
+++ c_src/from_label_deform.c
@@ -6,7 +6,7 @@
 void from_label_deform(double* label_x, double* label_y, int* label_dst_shape,
                        double* src_img_b, double* src_img_g, double* src_img_r, 
                        double* dst_img_b, double* dst_img_g, double* dst_img_r,
-                       int* src_img_shape) {
+                       int* src_img_shape, int* image_edge) {
     
     int dst_rows = label_dst_shape[0], dst_cols = label_dst_shape[1];
     int src_rows = src_img_shape[0], src_cols = src_img_shape[1];
@@ -15,6 +15,9 @@ void from_label_deform(double* label_x, double* label_y, int* label_dst_shape,
     int floor_x, ceil_x, floor_y, ceil_y;
     int offset_x = (int)(src_rows * 0.25);
     int offset_y = (int)(src_cols * 0.25);
+
+    int min_row = dst_rows, max_row = 0;
+    int min_col = dst_cols, max_col = 0;
 
     for(int i = 0; i < dst_rows; i++) {
         for(int j = 0; j < dst_cols; j++) {
@@ -27,6 +30,12 @@ void from_label_deform(double* label_x, double* label_y, int* label_dst_shape,
                 if((src_x < 0 || src_x >= src_rows - 1) || (src_y < 0 || src_y >= src_cols - 1))
                     continue;
                 else {
+                    
+                    min_row = i < min_row ? i : min_row;
+                    max_row = i > max_row ? i : max_row;
+                    min_col = j < min_col ? j : min_col;
+                    max_col = j > max_col ? j : max_col;
+
                     ceil_x = (int)ceil(src_x);
                     ceil_y = (int)ceil(src_y);
                     floor_x = (int)floor(src_x);
@@ -57,6 +66,8 @@ void from_label_deform(double* label_x, double* label_y, int* label_dst_shape,
             }
         }
     }
-
-
+    image_edge[0] = min_row;
+    image_edge[1] = max_row;
+    image_edge[2] = min_col;
+    image_edge[3] = max_col;
 }
