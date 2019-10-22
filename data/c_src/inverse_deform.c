44eb92a8d8c65f7fbec3d1a997080fda74499e8e
wuleiaty <1046828876@qq.com>
2019-10-22 20:20:56 +0800

    add interpolate

--- data/c_src/inverse_deform.c
+++ data/c_src/inverse_deform.c
@@ -4,7 +4,7 @@
 void inverse_deform(double* label_x, double* label_y, int* src_shape,
                     double* src_img_b, double* src_img_g, double* src_img_r,
                     double* dst_img_b, double* dst_img_g, double* dst_img_r,
-                    int* dst_shape, int* offset) {
+                    int* dst_shape, int* offset, int* mask) {
 
     int src_rows = src_shape[0], src_cols = src_shape[1];
     int dst_rows = dst_shape[0], dst_cols = dst_shape[1];
@@ -35,6 +35,8 @@ void inverse_deform(double* label_x, double* label_y, int* src_shape,
                     dst_img_g[floor_x * dst_cols + floor_y] = src_img_g[i * src_cols + j];
 
                     dst_img_r[floor_x * dst_cols + floor_y] = src_img_r[i * src_cols + j];
+
+                    mask[floor_x * dst_cols + floor_y] = 1;
                 }
             } 
         }
@@ -65,15 +67,49 @@ void get_edge(double* img, int* shape, int* edge) {
     edge[3] = max_y;
 }
 
-void interpolate(double* img_b, double* img_g, double* img_r, double* mask, int* shape) {
+void interpolate(double* img_b, double* img_g, double* img_r, int* mask, int* shape) {
     int rows = shape[0], cols = shape[1];
 
-    double sum = 0;
+    double sum_b = 0, sum_g = 0, sum_r = 0;
     int count = 0;
     
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
+    for(int i = 1; i < rows - 1; i++) {
+        for(int j = 1; j < cols - 1; j++) {
             if(mask[i * cols + j] >= -EPSINON && mask[i * cols + j] <= EPSINON) {
+                if(mask[(i - 1) * cols + j] == 1) {
+                    sum_b += img_b[(i - 1) * cols + j];
+                    sum_g += img_g[(i - 1) * cols + j];
+                    sum_r += img_r[(i - 1) * cols + j];
+                    count ++;
+                }
+                if(mask[(i - 1) * cols + (j - 1)] == 1) {
+                    sum_b += img_b[(i - 1) * cols + j - 1];
+                    sum_g += img_g[(i - 1) * cols + j - 1];
+                    sum_r += img_r[(i - 1) * cols + j - 1];
+                    count ++;
+
+                }
+                if(mask[(i + 1) * cols + j] == 1) {
+                    sum_b += img_b[(i + 1) * cols + j];
+                    sum_g += img_g[(i + 1) * cols + j];
+                    sum_r += img_r[(i + 1) * cols + j];
+                    count ++;
+                }
+                if(mask[(i + 1) * cols + j + 1] == 1) {
+                    sum_b += img_b[(i + 1) * cols + j + 1];
+                    sum_g += img_g[(i + 1) * cols + j + 1];
+                    sum_r += img_r[(i + 1) * cols + j + 1];
+                    count ++;
+                }
+
+                img_b[i * cols + j] = sum_b / count;
+                img_g[i * cols + j] = sum_g / count;
+                img_r[i * cols + j] = sum_r / count;
+
+                sum_b = 0;
+                sum_g = 0;
+                sum_r = 0;
+                count = 0;
 
             }
         }
