ba13afa9d0a96c8390ba3ddd1c2041f40c6826e3
wuleiaty <1046828876@qq.com>
2020-01-03 12:09:54 +0800

    stacked unet

--- model/model.py
+++ model/model.py
@@ -53,8 +53,38 @@ class Net(nn.Module):
 
         self.out_conv = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1, stride=1)
 
+        # second unet
+        self.down2_conv1 = Conv_block(66, 64)
+        self.down2_conv2 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            Conv_block(64, 128),
+        )
+        self.down2_conv3 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            Conv_block(128, 256),
+        )
+        self.down2_conv4 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            Conv_block(256, 512),
+        )
+        self.down2_conv5 = nn.Sequential(
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            Conv_block(512, 1024),
+        )
+        self.up2_conv1 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=2, stride=2)
+        self.up2_conv1_later = Conv_block(1024, 512)
+        self.up2_conv2 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=2, stride=2)
+        self.up2_conv2_later = Conv_block(512, 256)
+        self.up2_conv3 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=2, stride=2)
+        self.up2_conv3_later = Conv_block(256, 128)
+        self.up2_conv4 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
+        self.up2_conv4_later = Conv_block(128, 64)
+
+        self.out2_conv = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1, stride=1)
+
     def forward(self, x):
         features_stack = []
+        features_stack_2 = []
         x = self.down_conv1(x)
         features_stack.append(x)
         x = self.down_conv2(x)
@@ -83,9 +113,38 @@ class Net(nn.Module):
         x = torch.cat((features_stack.pop(), x), dim=1)
         x = self.up_conv4_later(x)
 
-        x = self.out_conv(x)
+        out1 = self.out_conv(x)
 
-        return x
+        x = torch.cat((x, out1), dim=1)
+        x = self.down2_conv1(x)
+        features_stack_2.append(x)
+        x = self.down2_conv2(x)
+        features_stack_2.append(x)
+        x = self.down2_conv3(x)
+        features_stack_2.append(x)
+        x = self.down2_conv4(x)
+        features_stack_2.append(x)
+        x = self.down2_conv5(x)
+        
+        x = self.up2_conv1(x)
+        x = torch.cat((features_stack_2.pop(), x), dim=1)
+        x = self.up2_conv1_later(x)
+
+        x = self.up2_conv2(x)
+        x = torch.cat((features_stack_2.pop(), x), dim=1)
+        x = self.up2_conv2_later(x)
+        
+        x = self.up2_conv3(x)
+        x = torch.cat((features_stack_2.pop(), x), dim=1)
+        x = self.up2_conv3_later(x)
+        
+        x = self.up2_conv4(x)
+        x = torch.cat((features_stack_2.pop(), x), dim=1)
+        x = self.up2_conv4_later(x)
+
+        out2 = self.out2_conv(x)
+        
+        return out2
 
 if __name__ == '__main__':
     # images_folder = '/home/wulei/DocUNet/data_gen/gen_test/image/'
@@ -111,4 +170,4 @@ if __name__ == '__main__':
     x = torch.randn((10, 3, 352, 272))
     net = Net()
     out = net(x)
-    print(x.size())
+    print(out.size())
