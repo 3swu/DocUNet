d196816a9e76da70c96b2e33a67ffde15d44b6a4
wuleiaty <1046828876@qq.com>
2020-01-14 19:38:25 +0800

    update

--- model/predict.py
+++ model/predict.py
@@ -1,13 +1,17 @@
+import sys
+sys.path.insert(0, '/home/wulei/DocUNet')
+
 import argparse
 import os
 
 import matplotlib.image as mpimg
+import numpy as np
 import torch
 import torch.nn as nn
 import torchvision.transforms
-import numpy as np
 
-from model import *
+from data.inverse_deform import inverse_deform
+from model import Net
 
 
 def get_args():
@@ -29,7 +33,7 @@ def predict_mesh(model: nn.Module, model_path, input_img, transforms):
     return output
 
 if __name__ == '__main__':
-    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     parser = get_args()
 
     model_path = parser.model_path
@@ -51,7 +55,18 @@ if __name__ == '__main__':
 
     output = output.cpu().numpy()
     label_x, label_y = output[0, :, :], output[1, :, :]
-    np.savez_compressed('./test.npz', x=label_x, y=label_y)
 
-    
-    
+    label = {
+        'x': label_x,
+        'y': label_y,
+    }
+    file_basename = os.path.basename(img_path)[:-4]
+    file_save_folder = '/home/wulei/DocUNet/test/' + file_basename
+    if not os.path.exists(file_save_folder):
+        os.mkdir(file_save_folder)
+    os.system('cp ' + img_path + ' ' + file_save_folder + '/' + file_basename + '.png')
+    os.system('cp ' + os.path.dirname(os.path.dirname(img_path)) + '/label/' + file_basename + '.npz' + ' ' + file_save_folder + '/' + file_basename + '.npz')
+    save_path = os.path.join(file_save_folder, file_basename + '-prediction.png')
+    np.savez_compressed(os.path.join(file_save_folder, file_basename + '-prediction.npz'), x=label_x, y=label_y)
+
+    inverse_deform(img_path, label, save_path)
